5edd5e470c4d0afec1fb8e92eb8cd3504f49ae6b
Akazhiel <jonatan.gonzalez.r@outlook.com>
2022-03-04 14:35:13 +0100

    Fixed error in vcfmerge.py.

--- scripts/vcfmerge.py
+++ scripts/vcfmerge.py
@@ -8,6 +8,6 @@ from scripts.common import exec_command
 
 def merge_variants(inp, out, distance):
     cmd = '{} --vcf {} --overlap 0.5 --ins_distance 5 --bnd_distance {} > {}'.format(
-        SVDB, ' '.join(inp), distance, distance, out
+        SVDB, ' '.join(inp), distance, out
     )
     exec_command(cmd)
