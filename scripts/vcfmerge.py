9252e505be0e27f17e9d741cd455e6af56b59dd6
Akazhiel <jonatan.gonzalez.r@outlook.com>
2022-03-21 16:11:21 +0100

    Modified merge on insertion distance.

--- scripts/vcfmerge.py
+++ scripts/vcfmerge.py
@@ -7,7 +7,7 @@ from scripts.common import exec_command
 
 
 def merge_variants(inp, out, distance):
-    cmd = '{} --vcf {} --overlap 0 --ins_distance 5 --no_intra --bnd_distance {} > {}'.format(
+    cmd = '{} --vcf {} --overlap 0 --ins_distance 1 --no_intra --bnd_distance {} > {}'.format(
         SVDB, ' '.join(inp), distance, out
     )
     exec_command(cmd)
