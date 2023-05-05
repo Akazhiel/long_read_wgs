5f86a4077347e25fc494887cad1a551cf287f1a3
Akazhiel <jonatan.gonzalez.r@outlook.com>
2023-05-05 09:30:03 +0200

    Revamped the filter step to perform merges in parallel in order to speed up this step.

--- main.py
+++ main.py
@@ -36,10 +36,10 @@ from scripts.vcfmerge import merge_variants
 def main(FQ_NORMAL, FQ_TUMOR, SAMPLEID, GENOME_REF, THREADS, STEPS, NUM_CALLERS, WINDOW, ENSEMBL_VERSION):
 
     logging.basicConfig(
-        format   = '[%(asctime)s] - [%(levelname)s] - %(message)s',
-        datefmt  = '%d-%b-%y %H:%M:%S',
-        level    = logging.DEBUG,
-        filename = SAMPLEID + '.log',
+        format='[%(asctime)s] - [%(levelname)s] - %(message)s',
+        datefmt='%d-%b-%y %H:%M:%S',
+        level=logging.DEBUG,
+        filename=SAMPLEID + '.log',
     )
     logger = logging.getLogger(SAMPLEID)
 
@@ -71,7 +71,8 @@ def main(FQ_NORMAL, FQ_TUMOR, SAMPLEID, GENOME_REF, THREADS, STEPS, NUM_CALLERS,
     if 'mapping' in STEPS:
 
         start_map_time = datetime.datetime.now()
-        logger.info('Starting trimming and mapping steps: {}'.format(start_map_time))
+        logger.info(
+            'Starting trimming and mapping steps: {}'.format(start_map_time))
 
         # TRIMMING To-Do
         logger.info('Started trimming')
@@ -107,7 +108,8 @@ def main(FQ_NORMAL, FQ_TUMOR, SAMPLEID, GENOME_REF, THREADS, STEPS, NUM_CALLERS,
 
         end_map_time = datetime.datetime.now()
         total_map_time = end_map_time - start_map_time
-        logger.info('Total trimming and mapping execution time: {}'.format(total_map_time))
+        logger.info(
+            'Total trimming and mapping execution time: {}'.format(total_map_time))
 
     if 'variant' in STEPS:
 
@@ -197,82 +199,67 @@ def main(FQ_NORMAL, FQ_TUMOR, SAMPLEID, GENOME_REF, THREADS, STEPS, NUM_CALLERS,
 
         end_variant_time = datetime.datetime.now()
         total_variant_time = end_variant_time - start_variant_time
-        logger.info('Total variant calling time: {}'.format(total_variant_time))
+        logger.info('Total variant calling time: {}'.format(
+            total_variant_time))
 
     if 'filter' in STEPS:
 
         start_filter_time = datetime.datetime.now()
-        logger.info('Starting filtering and reformatting: {}'.format(start_filter_time))
+        logger.info('Starting filtering and reformatting: {}'.format(
+            start_filter_time))
 
         # Reformat SVIM VCF to follow Sniffles format and filter on QUAL
 
-        p3 = mp.Process(
+        re_svimt = mp.Process(
             target=reformat_svim,
-            args=('svim_tumor/variants.vcf', 'tmp_svim_tumor.vcf', 'SVIM_Tumor', 10),
+            args=('svim_tumor/variants.vcf',
+                  'tmp_svim_tumor.vcf', 'SVIM_Tumor', 10),
         )
-        p4 = mp.Process(
+        re_svimn = mp.Process(
             target=reformat_svim,
-            args=('svim_normal/variants.vcf', 'tmp_svim_normal.vcf', 'SVIM_Normal', 1),
+            args=('svim_normal/variants.vcf',
+                  'tmp_svim_normal.vcf', 'SVIM_Normal', 1),
         )
 
-        p3.start()
-        p4.start()
+        re_svimt.start()
+        re_svimn.start()
 
-        p3.join()
-        p4.join()
-
-        # Re-genotype SVIM using Sniffles
-
-        # cmd = f'{SNIFFLES} --input {sample_tumor}.bam --allow-overwrite --genotype-vcf tmp_svim_tumor.vcf --quiet --vcf svim_tumor_regenotype.vcf -t {THREADS}'
-
-        # reg1 = exec_command(cmd, detach=True)
-
-        # cmd = f'{SNIFFLES} --input {sample_normal}.bam --allow-overwrite --genotype-vcf tmp_svim_normal.vcf --quiet --vcf svim_normal_regenotype.vcf -t {THREADS}'
-
-        # reg2 = exec_command(cmd, detach=True)
-
-        # reg1.wait()
-        # reg2.wait()
+        re_svimt.join()
+        re_svimn.join()
 
         # Filter variants in tumor vcf to keep only precise variants.
 
         cmd = f'{BCFTOOLS} view -i "INFO/STD_POS=\'.\'" tmp_svim_tumor.vcf > precise_svim_tumor.vcf'
-        p1 = exec_command(cmd, detach = True)
+        p1 = exec_command(cmd, detach=True)
 
         p1.wait()
 
-
         # Merge individual SVIM calls and filter for somatic variants
 
-        merge_variants(
-            ['precise_svim_tumor.vcf', 'tmp_svim_normal.vcf'], 'svim_combined_calls.vcf', 50
+        merge_svim = mp.Process(
+            target=merge_variants,
+            args=(['precise_svim_tumor.vcf', 'tmp_svim_normal.vcf'],
+                  'svim_combined_calls.vcf', 50)
         )
-        filter_somatic('svim_combined_calls.vcf', 'svim_combined_calls_filtered.vcf', 'SVIM')
 
-        # Reformat nanomonsv VCF to follow Sniffles format
-
-        # genomesv2vcf_convert('nanomon_vc/Tumor.nanomonsv.result.txt', 'Tumor.nanomonsv.result.vcf', GENOME_REF)
-
-        # reformat_nanomonsv('Tumor.nanomonsv.result.vcf', 'tmp_nanomonsv.vcf')
-
-        # filter_somatic('tmp_nanomonsv.vcf', 'nanomonsv_filtered.vcf', 'NANOMON')
+        merge_svim.start()
 
         # Reformat Sniffles to add INS Seq on INFO field
 
         cmd = f'{BCFTOOLS} view -i "STDEV_POS=0.0" {sample_tumor}_sniffles.vcf > precise_sniffles_tumor.vcf'
-        p6 = exec_command(cmd, detach=True)
+        p2 = exec_command(cmd, detach=True)
 
         # p5.wait()
-        p6.wait()
+        p2.wait()
 
-        p7 = mp.Process(
+        re_snifflesT = mp.Process(
             target=reformat_sniffles,
             args=(
                 '{}_sniffles.vcf'.format(sample_normal),
                 'tmp_sniffles_normal.vcf',
             ),
         )
-        p8 = mp.Process(
+        re_snifflesN = mp.Process(
             target=reformat_sniffles,
             args=(
                 'precise_sniffles_tumor.vcf',
@@ -280,49 +267,79 @@ def main(FQ_NORMAL, FQ_TUMOR, SAMPLEID, GENOME_REF, THREADS, STEPS, NUM_CALLERS,
             ),
         )
 
-        p7.start()
-        p8.start()
+        re_snifflesT.start()
+        re_snifflesN.start()
 
-        p7.join()
-        p8.join()
+        re_snifflesT.join()
+        re_snifflesN.join()
 
         # Merge individual SNIFFLES calls and filter for somatic variants
 
-        merge_variants(
-            ['tmp_sniffles_tumor.vcf', 'tmp_sniffles_normal.vcf'],
-            'sniffles_combined_calls.vcf',
-            50,
+        merge_sniffles = mp.Process(
+            target=merge_variants,
+            args=(['tmp_sniffles_tumor.vcf', 'tmp_sniffles_normal.vcf'],
+                  'sniffles_combined_calls.vcf', 50)
         )
 
-        filter_somatic(
-            'sniffles_combined_calls.vcf', 'sniffles_combined_calls_filtered.vcf', 'SNIFFLES'
-        )
+        merge_sniffles.start()
 
         # Reformat CuteSV
 
         cmd = f'{BCFTOOLS} view -i "CIPOS=0" CUTESV_Tumor.vcf > precise_cutesv_tumor.vcf'
-        p10 = exec_command(cmd, detach = True)
+        p3 = exec_command(cmd, detach=True)
 
         # p9.wait()
-        p10.wait()
+        p3.wait()
 
+        re_cutesvT = mp.Process(
+            target=reformat_cutesv,
+            args=('CUTESV_Normal.vcf', 'tmp_cutesv_normal.vcf')
+        )
 
-        p11 = mp.Process(target=reformat_cutesv, args=('CUTESV_Normal.vcf', 'tmp_cutesv_normal.vcf'))
-        p12 = mp.Process(target=reformat_cutesv, args=('precise_cutesv_tumor.vcf', 'tmp_cutesv_tumor.vcf'))
+        re_cutesvN = mp.Process(
+            target=reformat_cutesv,
+            args=('precise_cutesv_tumor.vcf', 'tmp_cutesv_tumor.vcf')
+        )
 
-        p11.start()
-        p12.start()
+        re_cutesvT.start()
+        re_cutesvN.start()
 
-        p11.join()
-        p12.join()
+        re_cutesvT.join()
+        re_cutesvN.join()
 
         # Merge individual CUTESV calls and filter for somatic variants
 
-        merge_variants(
-            ['tmp_cutesv_tumor.vcf', 'tmp_cutesv_normal.vcf'], 'cutesv_combined_calls.vcf', 50
+        merge_cutesv = mp.Process(
+            target=merge_variants,
+            args=(['tmp_cutesv_tumor.vcf', 'tmp_cutesv_normal.vcf'],
+                  'cutesv_combined_calls.vcf', 50)
         )
 
-        filter_somatic('cutesv_combined_calls.vcf', 'cutesv_combined_calls_filtered.vcf', 'CUTESV')
+        filter_somatic('cutesv_combined_calls.vcf',
+                       'cutesv_combined_calls_filtered.vcf', 'CUTESV')
+
+        # Wait for merges to finish
+
+        merge_svim.join()
+        merge_sniffles.join()
+        merge_cutesv.join()
+
+        # Filter somatic calls for each caller
+
+        filter_somatic('svim_combined_calls.vcf',
+                       'svim_combined_calls_filtered.vcf',
+                       'SVIM'
+                       )
+
+        filter_somatic('sniffles_combined_calls.vcf',
+                       'sniffles_combined_calls_filtered.vcf',
+                       'SNIFFLES'
+                       )
+
+        filter_somatic('cutesv_combined_calls.vcf',
+                       'cutesv_combined_calls_filtered.vcf',
+                       'CUTESV'
+                       )
 
         # Merge the combined calls from the different callers and filter based on number of callers
 
@@ -336,11 +353,13 @@ def main(FQ_NORMAL, FQ_TUMOR, SAMPLEID, GENOME_REF, THREADS, STEPS, NUM_CALLERS,
             WINDOW,
         )
 
-        filter_callers('combined_calls.vcf', 'combined_calls_filtered.vcf', NUM_CALLERS)
+        filter_callers('combined_calls.vcf',
+                       'combined_calls_filtered.vcf', NUM_CALLERS)
 
         end_filter_time = datetime.datetime.now()
         total_filter_time = end_filter_time - start_filter_time
-        logger.info('Total filtering and reformatting time: {}'.format(total_filter_time))
+        logger.info('Total filtering and reformatting time: {}'.format(
+            total_filter_time))
 
     if 'annotation' in STEPS:
 
@@ -358,7 +377,7 @@ def main(FQ_NORMAL, FQ_TUMOR, SAMPLEID, GENOME_REF, THREADS, STEPS, NUM_CALLERS,
 
         # Prirotize variants according to breakpoints.
     if 'epitope' in STEPS:
-        
+
         ensembl_data = EnsemblRelease(ENSEMBL_VERSION)
 
         annotsv_prio = prioritize_variants('annotsv_ensembl.tsv')
@@ -370,13 +389,15 @@ def main(FQ_NORMAL, FQ_TUMOR, SAMPLEID, GENOME_REF, THREADS, STEPS, NUM_CALLERS,
 
     end_pipeline_time = datetime.datetime.now()
     total_pipeline_time = end_pipeline_time - start_pipeline_time
-    logger.info('Total pipeline execution time: {}'.format(total_pipeline_time))
+    logger.info('Total pipeline execution time: {}'.format(
+        total_pipeline_time))
 
     logger.info('COMPLETED!')
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description=__doc__, formatter_class=RawDescriptionHelpFormatter)
+    parser = ArgumentParser(description=__doc__,
+                            formatter_class=RawDescriptionHelpFormatter)
     parser.add_argument('FASTQ_NORMAL', help='FASTQ file for Normal sample.')
     parser.add_argument('FASTQ_TUMOR', help='FASTQ file for Tumor sample')
     parser.add_argument(
@@ -443,25 +464,26 @@ if __name__ == '__main__':
         default=107,
         required=False,
     )
-    
+
     # parser.add_argument('--keep-intermediate', default=False, action='store_true', required=False,
     #                     help='Do not remove temporary files')
 
     # Parse arguments
-    args            = parser.parse_args()
-    DIR             = args.outdir
-    FQ_NORMAL       = os.path.abspath(args.FASTQ_NORMAL)
-    FQ_TUMOR        = os.path.abspath(args.FASTQ_TUMOR)
-    SAMPLEID        = args.sample
-    GENOME_REF      = os.path.abspath(args.genome)
-    THREADS         = int(args.threads)
-    STEPS           = args.steps
-    NUM_CALLERS     = args.num_callers
-    WINDOW          = args.window
+    args = parser.parse_args()
+    DIR = args.outdir
+    FQ_NORMAL = os.path.abspath(args.FASTQ_NORMAL)
+    FQ_TUMOR = os.path.abspath(args.FASTQ_TUMOR)
+    SAMPLEID = args.sample
+    GENOME_REF = os.path.abspath(args.genome)
+    THREADS = int(args.threads)
+    STEPS = args.steps
+    NUM_CALLERS = args.num_callers
+    WINDOW = args.window
     ENSEMBL_VERSION = args.ensembl_version
 
     # Move to output dir
     os.makedirs(os.path.abspath(DIR), exist_ok=True)
     os.chdir(os.path.abspath(DIR))
 
-    main(FQ_NORMAL, FQ_TUMOR, SAMPLEID, GENOME_REF, THREADS, STEPS, NUM_CALLERS, WINDOW, ENSEMBL_VERSION)
+    main(FQ_NORMAL, FQ_TUMOR, SAMPLEID, GENOME_REF, THREADS,
+         STEPS, NUM_CALLERS, WINDOW, ENSEMBL_VERSION)
