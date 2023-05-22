4f4b4477d8f30ef86ea49f7cbf56e6e5b20fafc7
Akazhiel <jonatan.gonzalez.r@outlook.com>
2023-05-22 12:56:15 +0200

    Reformatted filter_somatic to accept list DR field

--- scripts/filters.py
+++ scripts/filters.py
@@ -11,14 +11,14 @@ def filter_somatic(inp, out, caller):
     writer = vcfpy.Writer.from_path(out, reader.header)
     for record in reader:
         calls = {x.sample: x.data for x in record.calls}
-        token = calls[f'{caller}_Normal']['DR']
-        normal_DR = token if type(token) is int else 0
-        token = calls[f'{caller}_Tumor']['DR']
-        tumor_DR = token if type(token) is int else 0
-        token = calls[f'{caller}_Normal']['DV']
-        normal_DV = token if type(token) is int else 0
-        token = calls[f'{caller}_Tumor']['DV']
-        tumor_DV = token if type(token) is int else 0
+        token = calls[f'{caller}_Normal']['AD']
+        normal_DR = token if type(token) is int else token[0]
+        token = calls[f'{caller}_Tumor']['AD']
+        tumor_DR = token if type(token) is int else token[0]
+        token = calls[f'{caller}_Normal']['AD']
+        normal_DV = token if type(token) is int else token[1]
+        token = calls[f'{caller}_Tumor']['AD']
+        tumor_DV = token if type(token) is int else token[1]
         tumor_DP = tumor_DR + tumor_DV
         normal_DP = normal_DR + normal_DV
         tumor_VAF = round((tumor_DV/tumor_DP) * 100, 3) if tumor_DP != 0 else 0
@@ -32,7 +32,7 @@ def filter_callers(inp, out, num_callers):
     reader = vcfpy.Reader.from_path(inp)
     writer = vcfpy.Writer.from_path(out, reader.header)
     for record in reader:
-        called = [x for x in record.calls if x.data['DR'] is not None and 'Normal' not in x.sample]
+        called = [x for x in record.calls if x.data['AD'] is not None and 'Normal' not in x.sample]
         if len(called) >= num_callers:
             writer.write_record(record) 
 
